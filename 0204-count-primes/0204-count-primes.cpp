0545a91cfd3610172cad28d29b319779ceaebc08
Parv Aggarwal <143114920+Parvaggarwal01@users.noreply.github.com>
2025-10-23 15:31:40 +0530

    Time: 199 ms (44.3%), Space: 12.6 MB (91.19%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n == 0 || n == 1){
-            return 0;
-        }
-        vector<bool> isPrime(n+1, true);
+        if (n < 3) return 0;
 
-        isPrime[0] = isPrime[1] = false;
-        for(int i = 2; i*i<=n; i++){
-            if(isPrime[i] == true){
-                for(int j=i+i; j<=n; j+=i){
-                    isPrime[j] = false;
-                }
+        vector<bool> prime(n, false);
+
+        for (int x = 1; x * x < n; x++) {
+            for (int y = 1; y * y < n; y++) {
+                int num = 4 * x * x + y * y;
+                if (num < n && (num % 12 == 1 || num % 12 == 5))
+                    prime[num] = !prime[num];
+
+                num = 3 * x * x + y * y;
+                if (num < n && num % 12 == 7)
+                    prime[num] = !prime[num];
+
+                num = 3 * x * x - y * y;
+                if (x > y && num < n && num % 12 == 11)
+                    prime[num] = !prime[num];
             }
         }
-        int count = 0;
-        for(int i = 0; i<n; i++){
-            if(isPrime[i]){
-                count++;
+
+        for (int i = 5; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i * i)
+                    prime[j] = false;
             }
         }
+
+        // 2 and 3 are prime
+        int count = (n > 2) + (n > 3);
+        for (int i = 5; i < n; i++) {
+            if (prime[i]) count++;
+        }
+
         return count;
     }
 };
